99f8f826dcd02841fd66e9332c47560cc6551bd6
Melker Osterberg <melker.annat@gmail.com>
2018-10-07 17:16:31 +0200

    Improve install script

--- .dotfiles/.install-dotfiles.sh
+++ .dotfiles/.install-dotfiles.sh
@@ -12,24 +12,31 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
 else
   echo "Backing up pre-existing dot files.";
-dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
-fi;
+dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I % sh -c 'mkdir --parents .dotfiles-backup/%; mv % .dotfiles-backup/%;'
 dotfiles checkout
+fi;
 echo "Hiding untracked files.";
 dotfiles config status.showUntrackedFiles no
 
 echo "Creating Vim swap file directories.";
-mkdir $HOME/.vim/backup $HOME/.vim/swp $HOME/.vim/undo    #Create Vim swap file directories
+mkdir -p $HOME/.vim/backup $HOME/.vim/swp $HOME/.vim/undo    #Create Vim swap file directories
 chmod +x $HOME/.vim/backup $HOME/.vim/swp $HOME/.vim/undo #And make them executable (at least backup needs this)
+
+echo "Installing Vundle and Vundle plugins for Vim";
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+vim +PluginInstall +qa
 
 echo "Removing README.md and setting it to 'assume-unchanged'.";
 dotfiles update-index --assume-unchanged $HOME/README.md
-rm $HOME/README.md
+rm -f $HOME/README.md
 
 if [[ `cat $HOME/.gitignore 2>/dev/null` = ".dotfiles" ]]; then
   echo "Removing ~/.gitignore."
   rm $HOME/.gitignore
 fi
+
+echo "Installing Antigen";
+curl -L git.io/antigen > .zsh/antigen.zsh
 
 echo "Installing Powerline.";
 pip install --user powerline-status
