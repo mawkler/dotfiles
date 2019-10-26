6e0826e3734aaeccf87bbb78e95da52c41787290
Melker Österberg <melkerosterberg@gmail.com>
2019-10-26 14:14:21 +0200

    Install dependencies as user, not root

--- .dotfiles/install-dependencies.sh
+++ .dotfiles/install-dependencies.sh
@@ -1,8 +1,8 @@
 if type pacman &> /dev/null; then
   echo "Installing packages in 'pkglist.txt'";
-  pacman -S yay
-  sudo -i -u $USER yay -S --needed --noconfirm - < .dotfiles/pkglist.txt
+  yes | sudo -i -u $USER pacman -S yay
+  sudo -i -u $USER yay -S --needed --noconfirm - < ~/.dotfiles/pkglist.txt
 fi
 
 echo "Adding npm dependencies";
-npm install -gy yarn # For coc.nvim
+sudo -i -u $USER npm install -gy yarn # For coc.nvim
