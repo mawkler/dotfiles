0f9b54e1bfb3eb646c645c70e10e13b187448849
Melker Österberg <melkerosterberg@gmail.com>
2021-01-23 00:50:42 +0100

    Improve install script to properly backup dotfiles

--- .dotfiles/install-dotfiles.sh
+++ .dotfiles/install-dotfiles.sh
@@ -7,7 +7,7 @@
 cd
 echo ".dotfiles" >> $HOME/.gitignore
 
-echo "Cloning dotfiles repo.";
+echo "Cloning dotfiles repo."
 yes | git clone --bare git@github.com:Melkster/dotfiles.git $HOME/.dotfiles 2> /dev/null
 if [ $? -ne 0 ]; then # If cloning with SSH doesn't work, use HTTP
   git clone --bare https://github.com/Melkster/dotfiles.git $HOME/.dotfiles
@@ -16,21 +16,27 @@ fi
 function dotfiles {
   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p $HOME/.dotfiles-backup
+
+backup_dir="$HOME/.dotfiles-backup/"
+
+mkdir -p $backup_dir
 dotfiles checkout 2> /dev/null
 if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
+  echo "Checked out dotfiles."
 else
-  echo "Backing up pre-existing dot files.";
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I % sh -c 'mkdir --parents .dotfiles-backup/%; mv % .dotfiles-backup/%;'
-fi;
+  echo "Backing up pre-existing dotfiles."
+  files_to_backup=`dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'}`
+  echo $files_to_backup | xargs -n 1 | xargs -I {} sh -c 'mkdir -p ${2}$(dirname $1); mv $1 $2$1' _ {} $backup_dir
+  dotfiles checkout
+  echo "Checked out dotfiles. Pre-existing files backed up to $backup_dir."
+fi
 
-echo "Configuring repository.";
+echo "Configuring repository."
 dotfiles config status.showUntrackedFiles no
 dotfiles config --add remote.origin.fetch "refs/heads/*:refs/remotes/origin/*"
 dotfiles config --local push.default current # Should set upstream like `dotfiles push --set-upstream origin master` does, but without using `push`?
 
-echo "Removing README.md and setting it to 'assume-unchanged'.";
+echo "Removing README.md and setting it to 'assume-unchanged'."
 dotfiles update-index --assume-unchanged $HOME/README.md
 rm -f $HOME/README.md
 
