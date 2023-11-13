dfc51a6098e7ffe540d961ac4dfc2331e7c6ea06
Melker Ulander <melker.ulander@pm.me>
2023-11-13 13:51:34 +0100

    Remove packer.nvim
    I now use lazy.nvim, which gets installed by Neovim if missing

--- .dotfiles/install-configuration.sh
+++ .dotfiles/install-configuration.sh
@@ -4,34 +4,31 @@
 ## Installs configurations that don't require root privileges
 ###
 
-echo "Installing packer.nvim and Neovim plugins";
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerInstall'
+echo {} > $HOME/.cspell.json
 
-pip install --user autopep8 flake8 # For Python autoformatting and linting
+echo "Installing Python dependencies for Neovim"
 pip install --user neovim
 
-echo "Installing Antigen";
+echo "Installing Antigen"
 mkdir -p .zsh
 curl -L git.io/antigen > .zsh/antigen.zsh
 
 touch $HOME/.fzf_history # History file required by fzf-configuration in ~/.zshrc
 
-echo "Installing less configuration based on ~/.lesskey";
+echo "Installing less configuration based on ~/.lesskey"
 lesskey
 
 if [[ $XDG_CURRENT_DESKTOP == "GNOME" ]]; then
-  echo "Adding various Gnome settings"
-  dconf load /org/gnome/settings-daemon/plugins/media-keys/ < .dotfiles/media-keys.dconf
-  dconf load /org/gnome/desktop/wm/keybindings/ < .dotfiles/keybindings.dconf
-  dconf load /org/gnome/shell/extensions/ < .dotfiles/gnome-extensions.dconf
-  dconf load /org/gnome/shell/window-switcher/ < .dotfiles/window-switcher.dconf
-  dconf load /org/gnome/desktop/interface/ < .dotfiles/interface.dconf
-  dconf load /org/gnome/mutter/keybindings/ < .dotfiles/mutter-keybindings.dconf
+	echo "Adding various Gnome settings"
+	dconf load /org/gnome/settings-daemon/plugins/media-keys/ < .dotfiles/media-keys.dconf
+	dconf load /org/gnome/desktop/wm/keybindings/ < .dotfiles/keybindings.dconf
+	dconf load /org/gnome/shell/extensions/ < .dotfiles/gnome-extensions.dconf
+	dconf load /org/gnome/shell/window-switcher/ < .dotfiles/window-switcher.dconf
+	dconf load /org/gnome/desktop/interface/ < .dotfiles/interface.dconf
+	dconf load /org/gnome/mutter/keybindings/ < .dotfiles/mutter-keybindings.dconf
 
-  echo "To change Numix Frost to dark theme run the following:"
-  echo "sudo mv /usr/share/themes/Numix-Frost/gtk-3.20/gtk-dark.css /usr/share/themes/Numix-Frost/gtk-3.20/gtk.css"
+	echo "To change Numix Frost to dark theme run the following:"
+	echo "sudo mv /usr/share/themes/Numix-Frost/gtk-3.20/gtk-dark.css /usr/share/themes/Numix-Frost/gtk-3.20/gtk.css"
 fi
 
 echo "Installing FiraCode with stylistic set"
