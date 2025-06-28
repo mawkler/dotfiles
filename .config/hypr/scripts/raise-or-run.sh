723ace4eb6a64fb7593b101853003f02b06878d4
Melker Ulander <melker.ulander@pm.me>
2025-06-28 23:32:06 +0200

    Improve formatting of raise-or-run script

--- .config/hypr/scripts/raise-or-run.sh
+++ .config/hypr/scripts/raise-or-run.sh
@@ -1,35 +1,40 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [[ -z $1 ]] || [[ -z $2 ]]; then
   echo "Usage: $0 <program> <window_class>"
   exit 1
 fi
 
-PROGRAM="$1"
-WINDOW_CLASS="$2"
+PROGRAM=$1
+WINDOW_CLASS=$2
 
 WINDOWS=$(hyprctl -j clients)
 
 ACTIVE_WINDOW=$(hyprctl activewindow -j)
-ACTIVE_WINDOW_CLASS=$(echo "$ACTIVE_WINDOW" | jq -r '.class')
-ACTIVE_WINDOW_ADDRESS=$(echo "$ACTIVE_WINDOW" | jq -r '.address')
+ACTIVE_WINDOW_CLASS=$(echo $ACTIVE_WINDOW | jq -r '.class')
+ACTIVE_WINDOW_ADDRESS=$(echo $ACTIVE_WINDOW | jq -r '.address')
 
 # If we're already in a window of the program to switch to, then switch to its next window
-if [ "$ACTIVE_WINDOW_CLASS" == "$WINDOW_CLASS" ]; then
-  NEXT_WINDOW_ADDRESS=$(echo "$WINDOWS" | jq --raw-output --arg class "$WINDOW_CLASS" --arg address "$ACTIVE_WINDOW_ADDRESS" \
-    'map(select(.class == $class and .address != $address)) |
-    sort_by(.focusHistoryID) |
-    .[0] | .address')
+if [[ $ACTIVE_WINDOW_CLASS == $WINDOW_CLASS ]]; then
+  NEXT_WINDOW_ADDRESS=$(
+    echo $WINDOWS |
+      jq --raw-output --arg class $WINDOW_CLASS --arg address $ACTIVE_WINDOW_ADDRESS \
+        'map(
+          select(.class == $class and .address != $address)
+        ) | sort_by(.focusHistoryID) | .[0] | .address'
+  )
 
-  if [ -n "$NEXT_WINDOW_ADDRESS" ] && [ "$NEXT_WINDOW_ADDRESS" != "null" ]; then
-    hyprctl dispatch focuswindow "address:$NEXT_WINDOW_ADDRESS"
+  if [[ -n $NEXT_WINDOW_ADDRESS ]] && [[ $NEXT_WINDOW_ADDRESS != "null" ]]; then
+    hyprctl dispatch focuswindow address:$NEXT_WINDOW_ADDRESS
   fi
+
+  exit
+fi
+
+WINDOW_ADDRESS=$(echo $WINDOWS | jq --raw-output --arg class $WINDOW_CLASS \
+  'map(select(.class | contains($class))) | sort_by(.focusHistoryID) | .[0] | .address')
+if [[ -n $WINDOW_ADDRESS ]] && [[ $WINDOW_ADDRESS != "null" ]]; then
+  hyprctl dispatch focuswindow address:$WINDOW_ADDRESS
 else
-  WINDOW_ADDRESS=$(echo "$WINDOWS" | jq --raw-output --arg class "$WINDOW_CLASS" \
-    'map(select(.class | contains($class))) | sort_by(.focusHistoryID) | .[0] | .address')
-  if [ -n "$WINDOW_ADDRESS" ] && [ "$WINDOW_ADDRESS" != "null" ]; then
-    hyprctl dispatch focuswindow "address:$WINDOW_ADDRESS"
-  else
-    "$PROGRAM" &
-  fi
+  $PROGRAM &
 fi
