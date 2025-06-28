fd22de3f128d63a481ffea5d3f63dcde2bead821
Melker Ulander <melker.ulander@pm.me>
2025-06-28 23:25:12 +0200

    Switch to the active program's next window

--- .config/hypr/scripts/raise-or-run.sh
+++ .config/hypr/scripts/raise-or-run.sh
@@ -9,10 +9,27 @@ PROGRAM="$1"
 WINDOW_CLASS="$2"
 
 WINDOWS=$(hyprctl -j clients)
-WINDOW=$(echo "$WINDOWS" | jq --arg class "$WINDOW_CLASS" '.[] | select(.class | contains($class))')
 
-if [ -n "$WINDOW" ]; then
-  hyprctl dispatch focuswindow class:$WINDOW_CLASS
+ACTIVE_WINDOW=$(hyprctl activewindow -j)
+ACTIVE_WINDOW_CLASS=$(echo "$ACTIVE_WINDOW" | jq -r '.class')
+ACTIVE_WINDOW_ADDRESS=$(echo "$ACTIVE_WINDOW" | jq -r '.address')
+
+# If we're already in a window of the program to switch to, then switch to its next window
+if [ "$ACTIVE_WINDOW_CLASS" == "$WINDOW_CLASS" ]; then
+  NEXT_WINDOW_ADDRESS=$(echo "$WINDOWS" | jq --raw-output --arg class "$WINDOW_CLASS" --arg address "$ACTIVE_WINDOW_ADDRESS" \
+    'map(select(.class == $class and .address != $address)) |
+    sort_by(.focusHistoryID) |
+    .[0] | .address')
+
+  if [ -n "$NEXT_WINDOW_ADDRESS" ] && [ "$NEXT_WINDOW_ADDRESS" != "null" ]; then
+    hyprctl dispatch focuswindow "address:$NEXT_WINDOW_ADDRESS"
+  fi
 else
-  "$PROGRAM" &
+  WINDOW_ADDRESS=$(echo "$WINDOWS" | jq --raw-output --arg class "$WINDOW_CLASS" \
+    'map(select(.class | contains($class))) | sort_by(.focusHistoryID) | .[0] | .address')
+  if [ -n "$WINDOW_ADDRESS" ] && [ "$WINDOW_ADDRESS" != "null" ]; then
+    hyprctl dispatch focuswindow "address:$WINDOW_ADDRESS"
+  else
+    "$PROGRAM" &
+  fi
 fi
